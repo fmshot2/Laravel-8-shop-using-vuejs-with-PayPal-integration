16806e89499f1928b6cc9fec3a858d62af5cd15e
fmshot2 <femishotolaa@gmail.com>
2022-12-11 19:21:18 +0100

    updated main branch to current content

---
+++ database/migrations/2022_11_28_192745_create_product_attributes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attributes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('attribute_id')->unsigned()->references('id')->on('attributes');
+            $table->unsignedInteger('product_id')->references('id')->on('products');
+            $table->string('value');
+            $table->integer('quantity');
+            $table->decimal('price')->nullable();
+            // $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_attributes');
+    }
+}
